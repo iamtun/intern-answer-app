9bc035541080516304ad0062f2add4f1813b6e3a
iamtun <letuan19431791.iuh@gmail.com>
2022-12-23 09:36:33 +0700

    add progress load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 import Header from '@/Layout/Header'
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <Header/>
+    <NextNProgress color='blue'/>
+    <Header />
     <Component {...pageProps} />
   </>
 }
